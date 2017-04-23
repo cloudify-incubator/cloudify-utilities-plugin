4d699b39e6819ac2549d1e90298bd1b21847401c
earthmant <earthmant@icloud.com>
2017-04-23 10:33:35 +0300

    added wait for to dep create
    added dep create flake8.
    
    extended default timeout
    
    adding debug to see whats going on
    
    short timeout
    
    short timeout
    
    handled time diff muzar
    
    adding req dateutil....
    
    removing
    
    logging
    
    rearranging
    
    removed perc-f
    
    adding mytime
    
    cleaning up

--- cloudify_deployment_proxy/tasks.py
+++ cloudify_deployment_proxy/tasks.py
@@ -13,6 +13,7 @@
 #    * limitations under the License.
 
 import time
+from datetime import datetime
 
 from cloudify import ctx
 from cloudify import manager
@@ -41,23 +42,92 @@ def poll_with_timeout(pollster,
     current_time = time.time()
 
     while time.time() <= current_time + timeout:
-        ctx.logger.info('Polling client...')
+        ctx.logger.debug('Polling client...')
         if pollster(**pollster_args) != expected_result:
-            ctx.logger.debug(
-                'Still polling.')
+            ctx.logger.debug('Still polling.')
             time.sleep(interval)
         else:
-            ctx.logger.info(
-                'Polling succeeded.')
+            ctx.logger.info('Polling succeeded.')
             return True
 
     ctx.logger.error('Polling failed.')
     return False
 
 
-def all_dep_workflows_in_state_pollster(_client, _dep_id, _state):
+def chop_datetime(_date):
+    return _date.split('.')[0]
+
+
+def timestamp_diff(expected, actual):
+    T_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+    expected_timestamp = \
+        datetime.strptime(chop_datetime(expected),
+                          T_FORMAT)
+    actual_timestamp = \
+        datetime.strptime(chop_datetime(actual),
+                          T_FORMAT)
+    timestamp_diff = actual_timestamp-expected_timestamp
+    return (timestamp_diff).total_seconds()
+
+
+def all_dep_workflows_in_state_pollster(_client,
+                                        _dep_id,
+                                        _state):
     _execs = _client.executions.list(deployment_id=_dep_id)
     return all([str(_e['status']) == _state for _e in _execs])
+
+
+def dep_workflow_in_state_pollster(_client,
+                                   _dep_id,
+                                   _state,
+                                   _workflow_id,
+                                   _created_at):
+
+    exec_list_fields = \
+        ['status', 'workflow_id', 'created_at']
+
+    _execs = \
+        _client.executions.list(deployment_id=_dep_id,
+                                _include=exec_list_fields)
+
+    for _exec in _execs:
+        ctx.logger.info('Exec: {0}'.format(_exec))
+        if _exec.get('workflow_id') == _workflow_id and \
+                _exec.get('status') == _state and \
+                timestamp_diff(_created_at,
+                               _exec.get('created_at')) <= 1:
+            return True
+    return False
+
+
+def poll_workflow_after_execute(_timeout,
+                                _client,
+                                _dep_id,
+                                _state,
+                                _workflow_id,
+                                _created_at):
+
+    pollster_args = {
+        '_client': _client,
+        '_dep_id': _dep_id,
+        '_state': _state,
+        '_workflow_id': _workflow_id,
+        '_created_at': _created_at
+    }
+
+    ctx.logger.info('Polling: {0}'.format(pollster_args))
+
+    success = \
+        poll_with_timeout(
+            dep_workflow_in_state_pollster,
+            timeout=_timeout,
+            pollster_args=pollster_args)
+
+    if not success:
+        raise NonRecoverableError(
+            'Deployment not ready. Timeout: {0} seconds.'.format(_timeout))
+    return True
 
 
 @operation
@@ -140,7 +210,7 @@ def upload_blueprint(**_):
         config.get('blueprint_id', ctx.instance.id)
 
     try:
-        bp_upload_resource = \
+        bp_upload_response = \
             client.blueprints._upload(blueprint_id=bp_id,
                                       archive_location=bp_archive,
                                       application_file_name=app_name)
@@ -149,11 +219,12 @@ def upload_blueprint(**_):
 
     ctx.instance.runtime_properties['blueprint'] = {}
     ctx.instance.runtime_properties['blueprint']['id'] = \
-        bp_upload_resource.get('id')
+        bp_upload_response.get('id')
 
 
 @operation
 def create_deployment(**_):
+
     client = _.get('client') or manager.get_rest_client()
     config = _.get('resource_config') or \
         ctx.node.properties.get('resource_config')
@@ -165,6 +236,7 @@ def create_deployment(**_):
     dep_id = _.get('deployment_id') or \
         config.get('deployment_id', bp_id)
     inputs = _.get('inputs') or config.get('inputs', {})
+    timeout = _.get('timeout', 30)
 
     try:
         dp_create_response = \
@@ -175,4 +247,69 @@ def create_deployment(**_):
         raise NonRecoverableError(
             'Deployment create failed {0}.'.format(str(ex)))
 
-    ctx.logger.info('Output: {0}'.format(dp_create_response))
+    dep_created_at = dp_create_response.get('created_at')
+
+    ctx.instance.runtime_properties['deployment'] = {}
+    ctx.instance.runtime_properties['deployment']['id'] = \
+        dp_create_response.get('id')
+    ctx.instance.runtime_properties['deployment']['created_at'] = \
+        dep_created_at
+
+    return poll_workflow_after_execute(timeout,
+                                       client,
+                                       dep_id,
+                                       'terminated',
+                                       'create_deployment_environment',
+                                       dep_created_at)
+
+
+# @operation
+# def execute_install(**_):
+#     client = _.get('client') or manager.get_rest_client()
+#     config = _.get('resource_config') or \
+#         ctx.node.properties.get('resource_config')
+#     dep_id = _.get('deployment_id') or \
+#         config.get('deployment_id',
+#                    ctx.instance.runtime_properties['deployment']['id'])
+#     workflow_id = _.get('workflow_id')
+#     execution_args = _.get('executions_start_args')
+#     timeout = _.get('timeout', 180)
+
+#     try:
+#         ex_start_response = \
+#                 client.executions.start(deployment_id=dep_id,
+#                                         workflow_id=workflow_id,
+#                                         **execution_args)
+#     except CloudifyClientError as ex:
+#         raise NonRecoverableError(
+#             'Executions start failed {0}.'.format(str(ex)))
+
+#     ctx.logger.info('Output: {0}'.format(ex_start_response))
+
+#     ctx.instance.runtime_properties['executions'] = {}
+#     ctx.instance.runtime_properties['executions']['workflow_id'] = \
+#         ex_start_response.get('id')
+#     ctx.instance.runtime_properties['executions']['created_at'] = \
+#         ex_start_response.get('created_at')
+
+#     dep_created_at = ctx.instance.runtime_properties.get('deployment',
+#                                                          {}).get('created_at')
+
+#     pollster_args = {
+#         '_client': client,
+#         '_dep_id': dep_id,
+#         '_state': 'terminated',
+#         '_workflow_id': 'install',
+#         '_created_at': dep_created_at
+#     }
+
+#     success = \
+#         poll_with_timeout(
+#             dep_workflow_in_state_pollster,
+#             timeout=timeout,
+#             pollster_args=pollster_args)
+
+#     if not success:
+#         raise NonRecoverableError(
+#             'Deployment not ready. Timeout: {0} seconds.'.format(timeout))
+#     return True
