e557333589ef05f9191e13d4867957ce516d18cb
earthmant <earthmant@icloud.com>
2017-07-03 09:30:28 +0300

    upgrading 1.2.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugin',
-    version='1.2.4',
+    version='1.2.5',
     author='Gigaspaces.com',
     author_email='hello@getcloudify.org',
     description='Utilities for extending Cloudify',
