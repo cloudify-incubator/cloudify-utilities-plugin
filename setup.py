8c674caf202904becbf53d6e00273df4c2f48999
Michael Shnizer <michaels@gigaspaces.com>
2017-09-14 16:49:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setuptools.setup(
               'cloudify_terminal',
               'cloudify_configuration',
               'cloudify_files',
+              'cloudify_custom_workflow',
               'cloudify_cloudinit'],
     license='LICENSE',
     install_requires=[
