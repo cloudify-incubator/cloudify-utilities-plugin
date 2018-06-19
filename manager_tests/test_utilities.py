23ef4650e0acea71c6ade12c5c79d311d17215ee
earthmant <trammell@cloudify.co>
2018-06-19 12:49:41 +0300

    test
    try
    
    test
    
    go
    
    no exclusions

--- manager_tests/test_utilities.py
+++ manager_tests/test_utilities.py
@@ -27,8 +27,8 @@ class TestUtilities(TestLocal):
             raise
 
     def setUp(self):
+        os.environ['ECOSYSTEM_SESSION_PASSWORD'] = 'admin'
         super(TestUtilities, self).setUp('azure.yaml')
-
         if 'ECOSYSTEM_SESSION_MANAGER_IP' not in os.environ:
             self.manager_ip = 'localhost'
         os.environ['ECOSYSTEM_SESSION_MANAGER_IP'] = self.manager_ip
@@ -44,51 +44,56 @@ class TestUtilities(TestLocal):
         delete_dep_command = \
             'cfy deployments delete -f {0}'.format(blueprint_id)
         utils.execute_command(delete_dep_command)
-        return utils.get_secrets('agent_key_private') or \
-            utils.get_secrets('agent_key_public')
+        if not utils.get_secrets('agent_key_private') or not \
+                utils.get_secrets('agent_key_public'):
+            raise Exception(
+                'agent_key_private or agent_key_public not in secrets')
 
     def install_deployment_proxy_new(self, blueprint_id):
+        parent_id = '{0}-parent'.format(blueprint_id)
         utils.execute_command(
             'cfy blueprints upload cloudify_deployment_proxy/'
             'examples/test-blueprint.yaml -b {0}'.format(
-                blueprint_id))
+                parent_id))
         utils.create_deployment(
-            blueprint_id, inputs={'test_id': blueprint_id})
-        utils.execute_install(blueprint_id)
-        deployment_outputs = utils.get_deployment_outputs(
-            blueprint_id)
-        if 'output1' not in deployment_outputs:
-            return True
-        return False
+            parent_id, inputs={'test_id': blueprint_id})
+        utils.execute_install(parent_id)
+        rs = utils.get_deployment_resources_by_node_type_substring(
+            parent_id,
+            'cloudify.nodes.DeploymentProxy')
+        deployment_outputs = \
+            rs[0]['instances'][0]['runtime_properties']['deployment']
+        if 'output1' not in deployment_outputs['outputs']:
+            raise Exception(
+                'output1 not in {0}'.format(deployment_outputs))
 
     def install_deployment_proxy_external(self, blueprint_id):
-        blueprint_id_existing = '{0}-existing'.format(blueprint_id)
+        parent_id = '{0}-existing'.format(blueprint_id)
         utils.execute_command(
             'cfy blueprints upload cloudify_deployment_proxy/'
             'examples/test-blueprint-existing.yaml -b {0}'.format(
-                blueprint_id_existing))
+                parent_id))
         utils.create_deployment(
-            blueprint_id_existing,
+            parent_id,
             inputs={'test_id': blueprint_id})
-        utils.execute_install(blueprint_id_existing)
-        deployment_outputs = utils.get_deployment_outputs(
-            blueprint_id)
-        if 'output1' not in deployment_outputs:
-            return True
-        return False
+        utils.execute_install(parent_id)
+        rs = utils.get_deployment_resources_by_node_type_substring(
+            parent_id,
+            'cloudify.nodes.DeploymentProxy')
+        deployment_outputs = \
+            rs[0]['instances'][0]['runtime_properties']['deployment']
+        if 'output1' not in deployment_outputs['outputs']:
+            raise Exception(
+                'output1 not in {0}'.format(deployment_outputs))
 
     def install_blueprints(self):
         ssh_id = 'sshkey-{0}'.format(
             os.environ['CIRCLE_BUILD_NUM'])
         proxy_id = 'proxy-{0}'.format(
             os.environ['CIRCLE_BUILD_NUM'])
-
-        result = self.install_ssh_key(ssh_id) or \
-            self.install_deployment_proxy_new(proxy_id) or \
-            self.install_deployment_proxy_external(proxy_id)
-        if result:
-            raise Exception('Failed to execute blueprint: {0}'.format(
-                result))
+        self.install_ssh_key(ssh_id)
+        self.install_deployment_proxy_new(proxy_id)
+        self.install_deployment_proxy_external(proxy_id)
 
     def test_blueprints(self):
         utils.update_plugin_yaml(
