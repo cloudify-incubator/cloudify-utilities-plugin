fe90799ffb7be515927bbd403588fcc8cf36c15c
Isaac Shabtay <isaac@shabtay.com>
2019-07-28 00:22:16 -0700

    Bumped version to 1.15.2 and added changelog entries

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugin',
-    version='1.15.1',
+    version='1.15.2',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities for extending Cloudify',
