70475f2af25cad47dc7d49d3126c65a21b6f4f95
earthmant <trammell@cloudify.co>
2019-08-25 11:37:20 +0300

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugin',
-    version='1.15.2',
+    version='1.15.3',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities for extending Cloudify',
