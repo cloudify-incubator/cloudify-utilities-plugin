4bb09bfbc2fed559dfafb47a0b0f8008b49d46b8
jrzeszutek <jaroslaw@cloudify.co>
2021-01-15 15:06:42 +0100

    fixed some issues & added example blueprint - wip

--- cloudify_resources/tasks.py
+++ cloudify_resources/tasks.py
@@ -18,8 +18,19 @@ from cloudify.exceptions import NonRecoverableError
 
 
 RESOURCES_LIST_PROPERTY = 'resource_config'
+FREE_RESOURCES_LIST_PROPERTY = 'free_resources'
 RESERVATIONS_PROPERTY = 'reservations'
 SINGLE_RESERVATION_PROPERTY = 'reservation'
+
+
+def _refresh_source_and_target_runtime_props(ctx, **kwargs):
+    ctx.source.instance.refresh()
+    ctx.target.instance.refresh()
+
+
+def _update_source_and_target_runtime_props(ctx, **kwargs):
+    ctx.source.instance.update()
+    ctx.target.instance.update()
 
 
 @operation(resumable=True)
@@ -32,51 +43,69 @@ def create_list(ctx, **kwargs):
 
     ctx.logger.debug('Initializing resources list...')
     ctx.instance.runtime_properties[RESOURCES_LIST_PROPERTY] = resource_config
+    ctx.instance.runtime_properties[FREE_RESOURCES_LIST_PROPERTY] = resource_config
     ctx.instance.runtime_properties[RESERVATIONS_PROPERTY] = {}
 
 
 @operation(resumable=True)
 def create_list_item(ctx, **kwargs):
     ctx.logger.debug('Initializing list item...')
-    ctx.instance.runtime_properties[SINGLE_RESERVATION_PROPERTY] = {}
+    ctx.instance.runtime_properties[SINGLE_RESERVATION_PROPERTY] = None
 
 
 @operation(resumable=True)
 def reserve_list_item(ctx, **kwargs):
-    resources_list = ctx.target.instance.runtime_properties.get(RESOURCES_LIST_PROPERTY, [])
-    reservations = ctx.target.instance.runtime_properties.get(RESERVATIONS_PROPERTY, {})
-
-    if len(resources_list) == len(reservations.items()):
+    if not ctx.target.instance.runtime_properties.get(FREE_RESOURCES_LIST_PROPERTY, []):
         raise NonRecoverableError(
             'Reservation has failed, because there are no available resources right now.')
 
-    for index in reservations.values():
-        resources_list.pop(index)
+    _refresh_source_and_target_runtime_props(ctx)
 
-    ctx.source.instance.runtime_properties[SINGLE_RESERVATION_PROPERTY] = resources_list.pop()
-    ctx.target.instance.runtime_properties[RESERVATIONS_PROPERTY][ctx.instance.id] = \
+    free_resources = ctx.target.instance.runtime_properties.get(FREE_RESOURCES_LIST_PROPERTY)
+    reservations = ctx.target.instance.runtime_properties.get(RESERVATIONS_PROPERTY, {})
+
+    ctx.source.instance.runtime_properties[SINGLE_RESERVATION_PROPERTY] = \
+        free_resources.pop(0)
+
+    ctx.target.instance.runtime_properties[FREE_RESOURCES_LIST_PROPERTY] = free_resources
+
+    reservations[ctx.source.instance.id] = \
         ctx.source.instance.runtime_properties.get(SINGLE_RESERVATION_PROPERTY)
-    ctx.logger.debug('Reservation successful: {0}\nLeft resources: {1}'.format(
-        ctx.source.instance.runtime_properties.get('reservation'),
-        resources_list
+    ctx.target.instance.runtime_properties[RESERVATIONS_PROPERTY] = reservations
+
+    _update_source_and_target_runtime_props(ctx)
+
+    ctx.logger.debug('Reservation successful: {0}\
+            \nLeft resources: {1}\
+            \nReservations: {2}'.format(
+        ctx.source.instance.runtime_properties.get(SINGLE_RESERVATION_PROPERTY),
+        ctx.target.instance.runtime_properties.get(FREE_RESOURCES_LIST_PROPERTY),
+        ctx.target.instance.runtime_properties.get(RESERVATIONS_PROPERTY)
     ))
 
 
 @operation(resumable=True)
 def return_list_item(ctx, **kwargs):
+    free_resources = ctx.target.instance.runtime_properties.get(FREE_RESOURCES_LIST_PROPERTY, [])
     reservation = ctx.source.instance.runtime_properties.get(SINGLE_RESERVATION_PROPERTY, None)
-    reservations = ctx.target.instance.runtime_properties.get(RESERVATIONS_PROPERTY, {})
+    reservations = ctx.target.instance.runtime_properties.get(RESERVATIONS_PROPERTY, None)
 
     if not reservation or not reservations:
         return ctx.logger.debug('Nothing to do.')
     
-    ctx.target.instance.runtime_properties[RESERVATIONS_PROPERTY] = \
-        {k:v for k, v in reservations.items() if v != reservation}
-
-    ctx.logger.debug('{} has been returned back successfully to the resources list.'.format(
-        reservation
-    ))
-
-
+    _refresh_source_and_target_runtime_props(ctx)
     
+    reservations.pop(ctx.source.instance.id)
+    ctx.target.instance.runtime_properties[RESERVATIONS_PROPERTY] = reservations
 
+    free_resources.append(reservation)
+    ctx.target.instance.runtime_properties[FREE_RESOURCES_LIST_PROPERTY] = free_resources
+    
+    ctx.source.instance.runtime_properties[SINGLE_RESERVATION_PROPERTY] = None
+
+    _update_source_and_target_runtime_props(ctx)
+
+    ctx.logger.debug('{0} has been returned back successfully to the resources list.\nLeft resources: {1}'.format(
+        reservation,
+        ctx.target.instance.runtime_properties.get(FREE_RESOURCES_LIST_PROPERTY, [])
+    ))
