5f54c08c96aaa70d7967a34eb11452a4c0d15ace
EarthmanT <trammell@cloudify.co>
2021-08-11 15:46:19 -0400

    update rhel....

--- extra-packaging-instructions.sh
+++ extra-packaging-instructions.sh
@@ -4,4 +4,5 @@
 
 # wget https://files.pythonhosted.org/packages/d4/eb/18e5aa4171f2b0b8e3c73f1850f2d9d39dff3ff3ba1fc8625a932877c968/cffi-1.14.6-cp27-cp27m-manylinux1_x86_64.whl
 
-/py2env/bin/pip install cffi cryptography
+/py2env/bin/pip install cffi 
+
