1234be3d6ecce6535fab9f5f59b45c0c40dab7cc
EarthmanT <trammell@cloudify.co>
2021-08-11 12:35:19 -0400

    testing it remotely cuz it does not work on mac

---
+++ extra-packaging-instructions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# yum install wget -y
+
+# wget https://files.pythonhosted.org/packages/d4/eb/18e5aa4171f2b0b8e3c73f1850f2d9d39dff3ff3ba1fc8625a932877c968/cffi-1.14.6-cp27-cp27m-manylinux1_x86_64.whl
+
+/py2env/bin/pip install cffi
